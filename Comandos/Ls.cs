1388bfe6961da8338e4bc4b97deadf80601535a7
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-10-10 18:24:11 -0300

    Ls implementado, mas ainda com instabilidades ao navegar pelos diretorios.

--- Comandos/Ls.cs
+++ Comandos/Ls.cs
@@ -10,7 +10,60 @@ namespace UnixUtilities.Comandos {
 
         public static void List() {
             Console.Clear();
-            Console.WriteLine("rodando o código do ls");
+            string defaultDirectory = @"C:\Users\" + Environment.UserName;
+            //Console.Write("Esse é o diretorio atual no qual voce esta " + defaultDirectory);
+            bool rodando = true;
+            while (rodando) {
+                if(Directory.Exists(defaultDirectory)) {
+                    Console.WriteLine("O diretorio atual é " 
+                        + defaultDirectory);
+
+                    string[] files = Directory.GetFiles(defaultDirectory);
+                    Console.WriteLine("Arquivos no Diretorio");
+                    foreach (string file in files) {
+                        Console.WriteLine(file);
+
+                    }
+                    string[] directories = Directory.GetDirectories(defaultDirectory);
+                    Console.WriteLine("Pastas no Diretorio");
+                    foreach (string directory in directories) {
+                        Console.WriteLine(directory);
+                    }
+
+                    Console.WriteLine(@"Para retornar ao menu pressione 0.
+Para voltar um diretorio pressione ../
+Ou digite o nome de um diretório válido para navegar para ele.
+
+");
+
+                    string reply = Console.ReadLine();
+                    switch (reply) {
+                        case "0":
+                            rodando = false;
+                            break;
+                        case "../":
+
+                            string parentDiretory = Path.Combine(defaultDirectory, "..");
+                            if (Directory.Exists(parentDiretory)) {
+                                
+                                defaultDirectory =  parentDiretory;
+
+                            }
+
+                            break;
+
+                        default:
+                            if (Directory.Exists(reply)) {
+                                defaultDirectory = reply;
+                            }
+                            break;
+
+                    }
+
+
+                }
+            }
+            
 
 
         }
